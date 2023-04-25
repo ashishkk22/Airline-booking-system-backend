ace20706361676b7d7efa32cb62b8904588aea8b
ashishkk22 <ashishkachhadiya22@gmail.com>
2023-04-26 00:29:50 +0530

    Added city model and migrations

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
+const bodyParser = require("body-parser");
 
 const setupAndStartSever = async () => {
   //create the express server
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   const PORT = 3000;
   app.listen(PORT, () => {
